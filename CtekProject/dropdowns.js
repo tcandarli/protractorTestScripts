58343fbc839ccece3b269087fd6aba56397ed58c
Taner Candarli <tcandarli@gmail.com>
2018-10-05 17:33:18 +0200

    Add files via upload

--- CtekProject/dropdowns.js
+++ CtekProject/dropdowns.js
@@ -19,4 +19,38 @@ describe('Dropdowns', () => {
 
     });
 
+    it("ng-options skout", () => {
+        browser.waitForAngularEnabled(false);
+        browser.get("https://www.skout.com");
+        //getting all elements from dropdown
+        element.all(by.model("user.birthday.month")).first()
+            .all(by.tagName("option")).getText().then(function (txt) {
+                console.log(txt);
+            });
+        //select item with cssContainingText method
+        element.all(by.model("user.birthday.month")).first()
+            .all(by.cssContainingText('option', 'May')).click();
+        browser.sleep(3000)
+        //Select element by text
+        element(by.model("user.birthday.month"))
+            .element(by.xpath('option[.="April"]')).click();
+        browser.sleep(3000);
+        //select element by its value
+        element(by.model("user.birthday.month"))
+            .element(by.css('option[value="7"]')).click();
+        browser.sleep(3000);
+    });
+
+    fit("ng-options skout", () => {
+        browser.waitForAngularEnabled(false);
+
+        browser.get("https://www.skout.com");
+        //getting all elements from a dropdown print it
+        element.all(by.id("birthday-month")).first()  // Finding the dropdown
+            .all(by.tagName('option'))
+                .getText().then(function (textArray) {
+                    console.log(textArray);
+            });
+    });
+
 });
